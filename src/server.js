2627256f8f1075245785ca6b03bd70c153869941
YounTaekSeon <youneciohc@gmail.com>
2022-01-13 17:32:29 +0900

    url 추가 및 res

--- src/server.js
+++ src/server.js
@@ -4,9 +4,14 @@ const PORT = 4000;
 
 const app = express();
 
-const handleHome = () => console.log("Somebody is trying to go home.")
-
+const handleHome = (req, res) => {
+  return res.send("I still love you");
+}
+const handleLogin = (req, res) => {
+  return res.send({ message: "Login here." })
+}
 app.get("/", handleHome); // app.get("/", () => console.log("somebody is trying to go home."));
+app.get("/login", handleLogin)
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
